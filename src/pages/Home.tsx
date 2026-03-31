04a284a7ee6bc3617418f165927e5d3bded5b780
M Sakin <msakin@freshds.com>
2026-03-31 15:00:52 +0600

    Add Home component with Hero, Services, Work, and CTA

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import Hero from '../components/Hero';
+import Services from '../components/Services';
+import Work from '../components/Work';
+import CTA from '../components/CTA';
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+      <Services />
+      <Work />
+      <CTA />
+    </main>
+  );
+}
